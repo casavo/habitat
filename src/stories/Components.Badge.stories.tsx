d01eb832d994bd4cc6d72071d1b9daf9e025b989
Sergio Bergamo <sergio.bergamo@casavo.com>
2023-10-10 11:23:40 +0200

    Allow dynamic icon and children

--- src/stories/Components.Badge.stories.tsx
+++ src/stories/Components.Badge.stories.tsx
@@ -3,9 +3,40 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Badge } from "../components/Badge/Badge";
 import { HabitatTheme } from "../utils/theme.css";
+import { ReactNode } from "react";
+
+const BadgeIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M8.00568 1.33337L14.6667 5.03708V14.6667H1.33337V5.03708L8.00568 1.33337ZM8.00504 3.23921L3.00004 6.01754V13H13V6.01671L8.00504 3.23921Z" />
+    </svg>
+  );
+};
+
+const Icons: Record<string, ReactNode> = {
+  defaultIcon: <BadgeIcon />,
+  empty: undefined,
+};
 
 const meta: Meta<typeof Badge> = {
   argTypes: {
+    children: {
+      control: "text",
+      defaultValue: "Badge",
+      description: "select the text of the badge",
+    },
+    icon: {
+      control: "select",
+      defaultValue: undefined,
+      description:
+        "select a icon but inside code you can pass whatever icon you want",
+      options: Object.keys(Icons),
+    },
     status: {
       control: "select",
       defaultValue: undefined,
@@ -26,19 +57,6 @@ const meta: Meta<typeof Badge> = {
 export default meta;
 type Story = StoryObj<typeof Badge>;
 
-const BadgeIcon = () => {
-  return (
-    <svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M8.00568 1.33337L14.6667 5.03708V14.6667H1.33337V5.03708L8.00568 1.33337ZM8.00504 3.23921L3.00004 6.01754V13H13V6.01671L8.00504 3.23921Z" />
-    </svg>
-  );
-};
-
 /*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
  * See https://storybook.js.org/docs/react/api/csf
@@ -49,11 +67,12 @@ export const _Badge: Story = {
   render: ({ ...args }) => (
     <div className={HabitatTheme}>
       <div style={{ display: "flex", gap: "16px" }}>
-        <Badge status={args.status} theme={args.theme}>
-          Badge
-        </Badge>
-        <Badge status={args.status} theme={args.theme} icon={<BadgeIcon />}>
-          Badge
+        <Badge
+          status={args.status}
+          theme={args.theme}
+          icon={Icons[args.icon as keyof typeof Icons]}
+        >
+          <>{args.children ? args.children : "Badge"}</>
         </Badge>
       </div>
     </div>
