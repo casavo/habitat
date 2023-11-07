d8a75ba9f9af84901f75565e648e5d7c6fdc2f46
Salvatore Laisa <salvatore.laisa@casavo.com>
2023-11-07 11:38:31 +0100

    feat: add casavo favicon in storybook

---
+++ .storybook/manager.js
@@ -0,0 +1,4 @@
+const link = document.createElement("link");
+link.setAttribute("rel", "shortcut icon");
+link.setAttribute("href", "https://casavo.com/favicon/favicon.svg");
+document.head.appendChild(link);
