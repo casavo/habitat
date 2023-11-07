0eacb72d7f15736309645c2973af276f64195917
Salvatore Laisa <salvatore.laisa@casavo.com>
2023-11-07 11:39:07 +0100

    fix: styles

--- src/utils/theme.css.ts
+++ src/utils/theme.css.ts
@@ -3,8 +3,10 @@ import { createTheme } from "@vanilla-extract/css";
 import { mq } from "./mediaqueries";
 import tokens from "./tokens.json";
 
+type Fn = (v: number, k: string, i: number) => string;
+
 // Quick and dirty map utility for objects
-const objectMap = (obj: Object, fn: Function) =>
+const objectMap = (obj: object, fn: Fn) =>
   Object.fromEntries(Object.entries(obj).map(([k, v], i) => [k, fn(v, k, i)]));
 
 const colors = tokens.foundations.colors;
