dfd11bc153f3fd8d9e5765698ee6457a39235dc2
Salvatore Laisa <salvatore.laisa@casavo.com>
2023-11-23 09:52:58 +0100

    feat: checkbox component styling progress

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,10 +1,56 @@
+import type { CheckboxProps } from "react-aria-components";
 import { Checkbox as RACheckbox } from "react-aria-components";
+import { checkbox, wrapper } from "./Checkbox.css";
+import { Body, Caption } from "../Typography";
 
-export const Checkbox = () => {
+type Props = CheckboxProps & {
+  checked?: boolean;
+  children?: React.ReactNode;
+  disabled?: boolean;
+  error?: boolean;
+  message?: string;
+  value?: string;
+  variant?: "neutral" | "b2b" | "b2c";
+};
+
+export const Checkbox = ({
+  checked,
+  children,
+  disabled,
+  error,
+  message,
+  value,
+  variant,
+}: Props) => {
   return (
-    <RACheckbox>
-      <div className="checkbox"></div>
-      some text
+    <RACheckbox
+      isDisabled={disabled}
+      isSelected={checked}
+      className={wrapper}
+      value={value}
+    >
+      <div className={checkbox({ checked, disabled, variant })}>
+        {checked && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path
+              d="M4.35982 7.39202L2.87769 8.73317L6.5114 12.7572L13.5542 5.70712L12.1414 4.29291L6.58447 9.85565L4.35982 7.39202Z"
+              fill="white"
+            />
+          </svg>
+        )}
+      </div>
+      <Body>{children}</Body>
+      {error && (
+        <div>
+          <Caption>{message || "error message"}</Caption>
+        </div>
+      )}
     </RACheckbox>
   );
 };
--- src/stories/Components.Checkbox.stories.tsx
+++ src/stories/Components.Checkbox.stories.tsx
@@ -1,26 +1,78 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { Checkbox } from "./../components/Checkbox";
-import { HabitatTheme } from "./../utils/theme.css";
+import { StoryLayout } from "./components/StoryLayout";
 
 const meta: Meta<typeof Checkbox> = {
+  argTypes: {
+    checked: {
+      control: "boolean",
+      description: "toggle the state",
+    },
+    disabled: {
+      control: "boolean",
+      description: "toggle the standard disabled state",
+    },
+    error: {
+      control: "boolean",
+      description: "toggle the error state",
+    },
+    message: {
+      control: "text",
+      description: "the error message to display",
+    },
+    value: {
+      control: "text",
+      description: "the value of the checkbox option",
+    },
+    variant: {
+      control: "select",
+      defaultValue: "neutral",
+      description: "choose the branding variant",
+      options: ["neutral", "B2B", "B2C"],
+    },
+  },
+  args: {
+    checked: true,
+    disabled: false,
+    error: false,
+    message: "error message",
+    value: "hasElevator",
+    variant: "neutral",
+  },
   component: Checkbox,
   title: "Components/Checkbox",
 };
 
+const description = (
+  <>
+    A checkbox is a form element that allows the user to select one or more
+    options from a range of options.
+  </>
+);
+
 export default meta;
 type Story = StoryObj<typeof Checkbox>;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/react/api/csf
- * to learn how to use render functions.
- */
-
 export const _Checkbox: Story = {
-  render: () => (
-    <div className={HabitatTheme}>
-      <Checkbox />
-    </div>
+  render: ({ ...args }) => (
+    <StoryLayout
+      description={description}
+      importLine={'import { Checkbox } from "@casavo/habitat"'}
+      isComponent={true}
+      title="Components/Checkbox"
+      usage={"<Checkbox checked={false}>Has elevator</Checkbox>"}
+    >
+      <Checkbox
+        checked={args.checked}
+        disabled={args.disabled}
+        error={args.error}
+        message={args.message}
+        value={args.value}
+        variant={args.variant?.toLocaleLowerCase() as never}
+      >
+        Has elevator
+      </Checkbox>
+    </StoryLayout>
   ),
 };
