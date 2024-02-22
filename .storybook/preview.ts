2835e65dd0392fcbad46322ba1f413661c840fef
Salvatore Laisa <salvatore.laisa@casavo.com>
2024-02-22 11:01:54 +0100

    feat: adding the new brand section pages

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -13,7 +13,7 @@ const preview: Preview = {
     },
     options: {
       storySort: {
-        order: ["Docs", "Foundations", "Typography", "Components"],
+        order: ["Docs", "Brand", "Foundations", "Typography", "Components"],
       },
     },
     docs: {
