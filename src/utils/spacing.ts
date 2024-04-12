edfecef16c7a426016c9c23bcec6e1cd99be0526
Giovanni Visentini <giovanni.visentini@casavo.com>
2024-04-12 12:36:20 +0200

    feat: define the spaces / corners keys in a better way to have a more strong typing

--- src/utils/spacing.ts
+++ src/utils/spacing.ts
@@ -5,17 +5,15 @@ const _space = tokens.foundations.space;
 // Base spacing unit in pixels
 const BASE: number = 8;
 
-// The spacing values that we want to generate
-const values: Array<number> = Object.keys(_space).map(Number);
-
 // Get the type of the elements in the values array
-type SpacingKeys = (typeof values)[number];
+type SpacingKeys = keyof (typeof _space);
 
 type Spacing = {
   [K in SpacingKeys]: string;
 };
 
-export const space: Spacing = values.reduce((acc: Spacing, value) => {
-  acc[value] = `${BASE * (value / 100)}px`;
+export const space:Spacing = Object.entries(_space).reduce((acc: Spacing, [key,value]) => {
+  acc[key as SpacingKeys] = `${BASE * (value / 100)}px`;
   return acc;
-}, {});
+}, {} as Spacing)
+
