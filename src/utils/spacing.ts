9a6ca8f6282ab202fde34fe623db74ef7da061b5
Salvatore Laisa <salvatore.laisa@casavo.com>
2024-04-16 15:26:03 +0200

    feat: spacing values from token file

--- src/utils/spacing.ts
+++ src/utils/spacing.ts
@@ -2,18 +2,22 @@ import tokens from "./tokens.json";
 
 const _space = tokens.foundations.space;
 
-// Base spacing unit in pixels
-const BASE: number = 8;
-
 // Get the type of the elements in the values array
-type SpacingKeys = keyof (typeof _space);
+type SpacingKeys = keyof typeof _space;
+
+const values = [...Object.values(_space)].map(String);
+
+// TODO: I would like this to be a type that is a union of all the values in the _space object
+type SpacingValues = (typeof values)[number];
 
 type Spacing = {
-  [K in SpacingKeys]: string;
+  [K in SpacingKeys]: SpacingValues;
 };
 
-export const space:Spacing = Object.entries(_space).reduce((acc: Spacing, [key,value]) => {
-  acc[key as SpacingKeys] = `${BASE * (value / 100)}px`;
-  return acc;
-}, {} as Spacing)
-
+export const space: Spacing = Object.entries(_space).reduce(
+  (acc: Spacing, [key, value]) => {
+    acc[key as SpacingKeys] = `${value}px`;
+    return acc;
+  },
+  {} as Spacing
+);
