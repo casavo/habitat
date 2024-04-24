b69ab7ada3dc280236d5411f933f70c09612071e
Salvatore Laisa <salvatore.laisa@casavo.com>
2024-04-24 16:07:08 +0200

    chore: formatted code

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,14 +2,19 @@ import type { CheckboxProps } from "react-aria-components";
 import { Checkbox as CheckboxAria } from "react-aria-components";
 import { checkbox, messageStyle, wrapper } from "./Checkbox.css";
 import { Body } from "../Typography";
-import {vars} from "../../utils/theme.css.ts";
+import { vars } from "../../utils/theme.css.ts";
 
-type Props = React.PropsWithChildren<{
-  checked?: boolean;
-  disabled?: boolean;
-  error?: boolean;
-  message?: string;
-} & Omit<CheckboxProps,"isSelected"|"isDisabled"|"children"|"className">>  ;
+type Props = React.PropsWithChildren<
+  {
+    checked?: boolean;
+    disabled?: boolean;
+    error?: boolean;
+    message?: string;
+  } & Omit<
+    CheckboxProps,
+    "isSelected" | "isDisabled" | "children" | "className"
+  >
+>;
 
 export const Checkbox = ({
   checked,
@@ -17,11 +22,11 @@ export const Checkbox = ({
   disabled,
   error,
   message,
-    ...props
+  ...props
 }: Props) => {
   return (
     <CheckboxAria
-        {...props}
+      {...props}
       isDisabled={disabled}
       isSelected={checked}
       className={wrapper}
@@ -30,7 +35,8 @@ export const Checkbox = ({
         data-id="check-icon"
         className={checkbox({ checked, disabled, error })}
       >
-        {checked && <svg
+        {checked && (
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
@@ -39,15 +45,15 @@ export const Checkbox = ({
           >
             <path
               d="M4.35982 7.39202L2.87769 8.73317L6.5114 12.7572L13.5542 5.70712L12.1414 4.29291L6.58447 9.85565L4.35982 7.39202Z"
-              fill= {vars.colors.root.neutral[0]}
+              fill={vars.colors.root.neutral[0]}
             />
           </svg>
-        }
+        )}
       </div>
       <Body noMargin={true}>{children}</Body>
       {error && (
         <div className={messageStyle}>
-          <Body noMargin size="s" color={'root.red.600'}>
+          <Body noMargin size="s" color={"root.red.600"}>
             {message || "error message"}
           </Body>
         </div>
